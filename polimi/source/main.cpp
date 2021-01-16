dd6ae6b1b3670d1f6827d64a1351828b1c988872
AsusUBU <monner.maggots@gmail.com>
2021-01-16 18:15:59 +0100

    linker script, C support

--- polimi/source/main.cpp
+++ polimi/source/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <vector>
 
 #include "peripherals/Aes.h"
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    std::vector<float> values;
+    values.reserve(4);
+    for (int i = 0; i < 4; ++i) {
+        values.push_back(i*rand()%128-(i*5000-rand())%5);
+    }
     return 0;
 }
